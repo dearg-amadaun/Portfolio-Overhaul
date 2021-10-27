7e081357dbe0f1b2103e094d6f3c347b706a5511
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-10-27 00:29:44 -0400

    tweaked scripts to allow hyperlinks in carousel

--- js/templatemo-scripts.js
+++ js/templatemo-scripts.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
 
   // Image Pop Up
   $(".tm-gallery").magnificPopup({
-    delegate: "a", // child items selector, by clicking on it popup will open
+    delegate: "img", // child items selector, by clicking on it popup will open
     type: "image",
     gallery: { enabled: true }
   });
